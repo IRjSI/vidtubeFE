a67c3b18648c9801a014afd4de943047db8ecb38
IRjSI <rajusuthar0077@gmail.com>
2025-04-20 20:10:58 +0530

    Implement tweet editing and deletion functionality: add update and delete methods, enhance UI with form for editing tweets, and improve state management for better user experience.

--- src/pages/AllTweets.tsx
+++ src/pages/AllTweets.tsx
@@ -1,13 +1,68 @@
 import { AuthContext } from "@/context/authContext";
 import axios from "axios";
+import { PencilIcon, Trash2, X } from "lucide-react";
 import { useContext, useEffect, useState } from "react";
 
 const AllTweets = () => {
     const [tweets, setTweets] = useState<any[]>([]);
     const [myTweets, setMyTweets] = useState<any[]>([]);
     const [whose, setWhose] = useState('all');
+    const [updatedContent, setUpdatedContent] = useState('');
+    const [form, setForm] = useState(false);
+    const [reload, setReload] = useState(false);
+    const [editingTweetId, setEditingTweetId] = useState<string | null>(null);
+
     //@ts-ignore
     const { token } = useContext(AuthContext);
+
+    const deleteTweet = async ( id: string ) => {
+        try {
+            const response = await axios.delete(`${import.meta.env.VITE_BACKEND_URL}/tweets/delete/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+    
+            if (response.data.success) {
+                console.log(response.data.message);
+                setReload(prev => !prev)
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
+    const updateTweet = async ( e: any, id: string) => {
+        e.preventDefault();
+        try {
+            const response = await axios.patch(`${import.meta.env.VITE_BACKEND_URL}/tweets/update/${id}`,{
+                newContent: updatedContent
+            }, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+    
+            if (response.data.success) {
+                console.log(response.data.message);
+                setReload(prev => !prev)
+                setForm(false)
+                console.log(form);
+                setUpdatedContent('');
+            }
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const showForm = (id: string, content: string) => {
+        setEditingTweetId(id);
+        setUpdatedContent(content);
+    };
+    const closeForm = () => {
+        setEditingTweetId(null);
+    };
 
     useEffect(() => {
         if (!token) return;    
@@ -29,10 +84,10 @@ const AllTweets = () => {
                 },
                 })
                 .then((response) => setMyTweets(response.data.data))
-                .catch((error) => console.error('Error fetching videos:', error));
+                .catch((error) => console.error('Error fetching tweets:', error));
         }
         
-    }, [token, whose]);
+    }, [token, whose, reload]);
 
   return (
     <div>
@@ -60,6 +115,7 @@ const AllTweets = () => {
                             className="flex flex-col relative cursor-pointer rounded-lg bg-[#d8d8d8] text-black transition p-2 h-44 mt-8"
                         >
                             <div className="rounded-lg mb-2 overflow-y-auto no-scrollbar">
+                                <Trash2 className="absolute right-0 text-red-700" />
                                 <div className="">
                                     {tweet.content}
                                 </div>
@@ -76,11 +132,29 @@ const AllTweets = () => {
                             key={ind}
                             className="flex flex-col relative cursor-pointer rounded-lg bg-[#d8d8d8] text-black transition p-2 h-56 mt-8"
                         >
-                            <div className="rounded-lg mb-2 overflow-y-auto no-scrollbar">
-                                <div className="">
-                                    {tweet.content}
+                            {editingTweetId === tweet._id ? (
+                                <div>
+                                    <X className="absolute right-8 text-blue-700" onClick={closeForm}/>
+                                    <form onSubmit={(e) => updateTweet(e,tweet._id)} className={`flex flex-col justify-center items-center gap-4 absolute backdrop-blur-md text-white`}>
+                                        <textarea 
+                                            value={updatedContent}
+                                            onChange={(e) => setUpdatedContent(e.target.value)}
+                                            className="bg-gray-700 p-4 rounded-md"
+                                            autoFocus
+                                        />
+                                        <button type="submit" className="bg-blue-700 p-2 rounded-md">Update</button>
+                                    </form>
                                 </div>
-                            </div>
+                            ) : (
+                                <div className="rounded-lg mb-2 overflow-y-auto no-scrollbar">
+                                    <PencilIcon className="absolute right-8 text-blue-700" onClick={() => showForm(tweet._id, tweet.content)}/>
+                                    <Trash2 className="absolute right-0 text-red-700" onClick={() => deleteTweet(tweet._id)} />
+                                    <div className="">
+                                        {tweet.content}
+                                    </div>
+                                </div>
+                            )}
+                            
                         </div>
                     );
                     })}
